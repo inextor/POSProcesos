e48f25725869782d67250e02a49bf018c4910206
nextor <nextor@gmail.com>
2023-11-29 15:35:40 -0800

    It compiles

--- src/app/pages/save-production-area/save-production-area.component.ts
+++ src/app/pages/save-production-area/save-production-area.component.ts
@@ -1,25 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BaseComponent } from '../../modules/shared/base/base.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, withFetch } from '@angular/common/http';
 import { RestService } from '../../modules/shared/services/rest.service';
 import { Production_Area } from '../../modules/shared/RestModels';
 import { RestSimple } from '../../modules/shared/Rest';
+import { GetEmpty } from '../../modules/shared/GetEmpty';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { mergeMap, of } from 'rxjs';
 
 @Component({
 	selector: 'app-save-production-area',
 	standalone: true,
-	imports: [CommonModule,HttpClientModule],
+	imports: [CommonModule,HttpClientModule,FormsModule],
 	templateUrl: './save-production-area.component.html',
 	styleUrl: './save-production-area.component.css'
 })
 
-export class SaveProductionAreaComponent
+export class SaveProductionAreaComponent implements OnInit
 {
-    production_area_rest: RestSimple<Production_Area>;
+	production_area_rest: RestSimple<Production_Area>;
+	production_area = GetEmpty.production_area();
+	is_loading:boolean = false;
 
-	constructor(private rest:RestService)
+	constructor(private rest:RestService,private route:ActivatedRoute,private router:Router)
 	{
 		this.production_area_rest = rest.initRestSimple<Production_Area>('production_area');
+	}
+
+	ngOnInit()
+	{
+		this.route.paramMap.pipe
+		(
+			mergeMap((paramMap)=>{
+				return paramMap.has('id')
+					? this.production_area_rest.get( paramMap.get('id' ) )
+					: of( GetEmpty.production_area() );
+			})
+		).subscribe((production_area)=>
+		{
+			this.production_area = production_area;
+		});
+	}
+
+	save(evt:Event)
+	{
+		evt.preventDefault();
+
+		this.is_loading = true;
+
+		this.production_area_rest
+		.create( this.production_area )
+		.subscribe((production_area)=>
+		{
+			//location.back();
+		})
 	}
 }
