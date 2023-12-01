791224132b0340bb8e6a23b0ad8200c29d8990c0
nextor <nextor@gmail.com>
2023-11-30 16:20:00 -0800

    It runs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { SaveProductionAreaComponent } from './pages/save-production-area/save-production-area.component';
 import { ListProductionAreaComponent } from './pages/list-production-area/list-production-area.component';
+import { ViewProductionAreaComponent } from './pages/view-production-area/view-production-area.component';
+import { SaveProcessComponent } from './pages/save-process/save-process.component';
 
 export const routes: Routes = [
   {path: '', component: ListProductionAreaComponent},
   {path: 'add-production-area', component: SaveProductionAreaComponent},
-  {path: 'edit-production-area', component: SaveProductionAreaComponent},
+  {path: 'edit-production-area/:id', component: SaveProductionAreaComponent},
   {path: 'list-production-area', component: ListProductionAreaComponent},
+  {path: 'view-production-area/:id', component: ViewProductionAreaComponent},
+  {path: 'add-process/:production_area_id', component: SaveProcessComponent},
+  {path: 'edit-process/:id', component: SaveProcessComponent },
 ];
