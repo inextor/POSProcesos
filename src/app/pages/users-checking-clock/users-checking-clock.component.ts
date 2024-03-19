6aacc6053f762f03f8f5fb95ba0b5b44247c8a3d
jorjeGs <jgs_23072000@outlook.com>
2024-03-19 14:48:26 -0700

    Cambio de interfaz/logica para lista de asistencia

--- src/app/pages/users-checking-clock/users-checking-clock.component.ts
+++ src/app/pages/users-checking-clock/users-checking-clock.component.ts
@@ -4,17 +4,21 @@ import { User,Check_In } from '../../modules/shared/RestModels';
 import { mergeMap, of, forkJoin } from 'rxjs';
 import { RestResponse } from '../../modules/shared/services/Rest';
 import { BaseComponent } from '../../modules/shared/base/base.component';
+import { FormsModule } from '@angular/forms';
 
 export interface UserCheckInfo
 {
 	user:User;
 	current_check_in:Check_In | null;
+	ordinary_hours: number;
+	extra_hours: number;
+	permission_hours: number;
 }
 
 @Component({
 	selector: 'app-users-checking-clock',
 	standalone: true,
-	imports: [CommonModule],
+	imports: [CommonModule, FormsModule],
 	templateUrl: './users-checking-clock.component.html',
 	styleUrl: './users-checking-clock.component.css'
 })
@@ -23,6 +27,7 @@ export class UsersCheckingClockComponent extends BaseComponent implements OnInit
 	user_checkin_info_list:UserCheckInfo[] = [];
 	rest_user = this.rest.initRestSimple<User>('user',['id','username','name','email','type']);
 	rest_check_in = this.rest.initRestSimple<Check_In>('check_in',['id','current']);
+	current_date = new Date();
 
 	ngOnInit()
 	{
@@ -30,10 +35,10 @@ export class UsersCheckingClockComponent extends BaseComponent implements OnInit
 		this.subs.sink = this.route.paramMap.pipe
 		(
 			mergeMap((param_map)=>
-			{
-				this.is_loading = true;
+			{	this.is_loading = true;
 				let search_obj = this.rest_user.getSearchObject(param_map);
 				search_obj.eq.type = 'USER';
+				search_obj.eq.store_id = this.rest.user?.store_id;
 
 				return this.rest_user.search(search_obj);
 			}),
@@ -67,7 +72,7 @@ export class UsersCheckingClockComponent extends BaseComponent implements OnInit
 				for(let user of response.users.data)
 				{
 					let current_check_in = response. check_ins. data. find(checkin=>user.id == checkin.user_id ) || null;
-					result.push({ user, current_check_in });
+					result.push({ user, current_check_in, ordinary_hours: 0, extra_hours: 0, permission_hours: 0});
 				}
 
 				return of(result)
@@ -97,5 +102,20 @@ export class UsersCheckingClockComponent extends BaseComponent implements OnInit
 				ucil.current_check_in = null;
 			}
 		},(error)=> this.showError( error ))
+	}
+
+	endTurn()
+	{
+		//mostar un modal para confirmar que se cerrara el turno
+		this.confirmation.showConfirmAlert(this.user_checkin_info_list,'Cerrar turno','¿Está seguro que desea cerrar el turno?')
+		.subscribe((response)=>
+		{
+			if(response.accepted)
+			{
+				//registra las horas de cada usuario para el dia actual
+				this.showSuccess('Se han registrado las horas de los usuarios (proximamente)')
+			}
+		});
+		
 	}
 }
