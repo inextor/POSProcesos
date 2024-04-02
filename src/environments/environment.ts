89407918de8fd98c0054fca9e832a934c49eb24e
jorjeGs <jgs_23072000@outlook.com>
2024-04-02 15:31:33 -0700

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,8 @@
 export const environment = {
     production: false,
     app_settings: {
-	    test_url: 'http://192.168.10.165',
+	    //test_url: 'http://192.168.10.165',
         path_api: 'PointOfSale',
+        test_url: 'http://127.0.0.1:80'
     }
 };
