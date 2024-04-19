1ac8a0edbe9d28832af39b7e6eeee838bfa41559
nextor <nextor@gmail.com>
2024-04-19 15:02:50 -0700

    Agregando los dos componentes

---
+++ src/app/pages/save-payroll/save-payroll.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-save-payroll',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './save-payroll.component.html',
+  styleUrl: './save-payroll.component.css'
+})
+export class SavePayrollComponent {
+
+}
