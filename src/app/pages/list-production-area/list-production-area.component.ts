1381a34bb784240a75accfb2da7e49cb21039e83
jorjeGs <jgs_23072000@outlook.com>
2024-06-08 09:58:37 -0700

    fix list production area

--- src/app/pages/list-production-area/list-production-area.component.ts
+++ src/app/pages/list-production-area/list-production-area.component.ts
@@ -1,13 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { GetEmpty } from '../../modules/shared/GetEmpty';
 import { RestSimple } from '../../modules/shared/services/Rest';
 import { Production_Area } from '../../modules/shared/RestModels';
-import { RestService } from '../../modules/shared/services/rest.service';
-import { ActivatedRoute, Router, RouterModule } from '@angular/router';
-import { mergeMap, Observable, Subscription } from 'rxjs';
+import { RouterModule } from '@angular/router';
+import { mergeMap } from 'rxjs';
 import { BaseComponent } from '../../modules/shared/base/base.component';
-import {Location} from '@angular/common';
 
 @Component({
 	selector: 'app-list-production-area',
@@ -16,25 +13,19 @@ import {Location} from '@angular/common';
 	templateUrl: './list-production-area.component.html',
 	styleUrl: './list-production-area.component.css'
 })
-export class ListProductionAreaComponent
+export class ListProductionAreaComponent extends BaseComponent implements OnInit
 {
 	production_area_rest: RestSimple<Production_Area> = this.rest.initRestSimple<Production_Area>('production_area');
 	production_area_list:Production_Area[] = [];
-	is_loading:boolean = false;
-	obs: Subscription | null = null;
-
-	constructor(private rest:RestService,private route:ActivatedRoute,private router:Router)
-	{
-	}
 
 	ngOnInit()
-	{
-		//this.subs.sync =
-		this.obs = this.route.paramMap.pipe
+	{	
+		this.is_loading = true;
+		this.subs.sink = this.route.paramMap.pipe
 		(
 			mergeMap((_paramMap)=>
 			{
-				return this.production_area_rest.search({})
+				return this.production_area_rest.search({limit:99999})
 			}),
 		)
 		.subscribe((response)=>
@@ -42,13 +33,6 @@ export class ListProductionAreaComponent
 			this.is_loading = false;
 			this.production_area_list = response.data;
 		})
-	}
-
-	ngOnDestroy()
-	{
-		if( this.obs )
-			this.obs.unsubscribe();
-		//this.subs.unsubscribe();
 	}
 
 }
