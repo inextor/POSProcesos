977fa68e4e033b3e81be99952c6500b0a97547c7
jorjeGs <jgs_23072000@outlook.com>
2024-07-04 15:23:16 -0700

    loading component

---
+++ src/app/components/loading/loading.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-loading',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './loading.component.html',
+  styleUrl: './loading.component.css'
+})
+export class LoadingComponent implements OnInit {
+	@Input() is_loading: boolean = false;
+
+	constructor() { }
+
+	ngOnInit() {
+	}
+}
