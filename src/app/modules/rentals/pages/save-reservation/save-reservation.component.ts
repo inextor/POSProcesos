1b31ff0ae2bbbdd3195227adf4687ed9c0f91146
nextor <nextor@gmail.com>
2024-08-05 13:06:05 -0700

    Adding empty files for list and save rentals

---
+++ src/app/modules/rentals/pages/save-reservation/save-reservation.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-save-reservation',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './save-reservation.component.html',
+  styleUrl: './save-reservation.component.css'
+})
+export class SaveReservationComponent {
+
+}
--- src/app/modules/rentals/rentals-routing.module.ts
+++ src/app/modules/rentals/rentals-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RentalsComponent } from './rentals.component';
+import { ListReservationComponent } from './pages/list-reservation/list-reservation.component';
 
-const routes: Routes = [{ path: '', component: RentalsComponent }];
+const routes: Routes =
+[
+	{ path: '', component: RentalsComponent }
+	,{ path: 'list-reservation', component: ListReservationComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
