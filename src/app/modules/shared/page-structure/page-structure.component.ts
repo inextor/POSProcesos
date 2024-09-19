2e4e850029dbcb38321d4274ee855f427acb1bf7
nextor <nextor@gmail.com>
2024-09-19 10:57:39 -0700

    Adding implementation o basic page structure to handle animation on menu open and to set a structur for the menu, header and the content of any page

--- src/app/modules/shared/page-structure/page-structure.component.ts
+++ src/app/modules/shared/page-structure/page-structure.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from "../../../components/header/header.component";
+import { HeadBodyFooterLayoutComponent } from "../components/head-body-footer-layout/head-body-footer-layout.component";
+import { BaseComponent } from '../base/base.component';
 
 @Component({
     selector: 'app-page-structure',
     standalone: true,
     templateUrl: './page-structure.component.html',
     styleUrl: './page-structure.component.css',
-    imports: [CommonModule, HeaderComponent]
+    imports: [CommonModule, HeaderComponent, HeadBodyFooterLayoutComponent]
 })
-export class PageStructureComponent {
+export class PageStructureComponent extends BaseComponent
+{
 
 }
