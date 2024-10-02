40799451a9ae667875f5a652fbf9b7679f7227d2
nextor <nextor@gmail.com>
2024-10-02 11:24:01 -0700

    Se agrego home como el inicio

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PageStructureComponent } from "../../modules/shared/page-structure/page-structure.component";
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { BaseComponent } from '../../modules/shared/base/base.component';
+
+@Component({
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    imports: [CommonModule, PageStructureComponent,RouterModule, RouterOutlet]
+})
+export class HomeComponent extends BaseComponent implements OnInit
+{
+
+	ngOnInit(): void
+	{
+		if( !this.rest.user?.id )
+		{
+			this.router.navigate(['/login']);
+		}
+	}
+}
