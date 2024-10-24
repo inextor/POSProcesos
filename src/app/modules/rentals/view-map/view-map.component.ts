934cd3131c60ba6016c132d63e742b8b444f8ec3
nextor <nextor@gmail.com>
2024-10-24 14:41:41 -0700

    Adding fu

---
+++ src/app/modules/rentals/view-map/view-map.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BaseComponent } from '../../shared/base/base.component';
+
+
+declare function initMap(lat:number,lng:number):Promise<any>;
+declare function addMarker(lat:number,lng:number,title:string):Promise<any>;
+
+@Component
+({
+	selector: 'app-view-map',
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: './view-map.component.html',
+	styleUrl: './view-map.component.css'
+})
+export class ViewMapComponent extends BaseComponent
+{
+	lng: number = 0;
+	lat: number = 0;
+	title: string = '';
+
+	ngOnInit()
+	{
+		this.subs.sink = this.route.paramMap.subscribe(params =>
+		{
+			this.title = params.get('title') as string;
+			this.setTitle( this.title );
+			this.lat = parseFloat(params.get('lat') as string);
+			this.lng = parseFloat(params.get('lng') as string);
+		});
+	}
+
+	ngAfterViewInit()
+	{
+		initMap(this.lat, this.lng)
+		.then(()=>
+		{
+			console.log('Mapa cargado');
+			return addMarker(this.lat, this.lng, this.title);
+		})
+		.then(()=>
+		{
+			console.log('marker agregado');
+			console.log('Mapa cargado');
+		})
+		.catch(err=>
+		{
+			console.log('Error cargando mapa');
+			console.log(err);
+		});
+	}
+}
---
+++ src/assets/js/maps.js
@@ -0,0 +1,33 @@
+(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
+	key: "AIzaSyC2RzI_wHQBIysXsI-g_Miwg2J1-o14cn8",
+	v: "quarterly",
+	// Use the 'v' parameter to indicate the version to use (quarterly,weekly, beta, alpha, etc.).
+	// Add other bootstrap parameters as needed, using camel case.
+});
+
+
+let map;
+
+async function initMap(lat, lng)
+{
+	const { Map } = await google.maps.importLibrary("maps");
+	const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+	let position = { lat, lng };
+
+	// The map, centered at Uluru
+	map = new Map(document.getElementById("map"), {
+		zoom: 4,
+		center: position,
+		mapId: "DEMO_MAP_ID",
+	});
+ }
+
+async function addMarker(lat, lng, title)
+{
+	const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+	let position = { lat, lng };
+	// The marker, positioned at Uluru
+	const marker = new AdvancedMarkerElement({ map, position, title });
+
+}
