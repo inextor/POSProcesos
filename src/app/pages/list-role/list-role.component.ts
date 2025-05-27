c2a764e3a73e67f259fdf38bdf6216852c66b220
nextor <nextor@gmail.com>
2025-05-27 12:13:49 -0700

    Formating

--- src/app/pages/list-role/list-role.component.ts
+++ src/app/pages/list-role/list-role.component.ts
@@ -6,29 +6,32 @@ import { Role } from '../../modules/shared/RestModels';
 import { mergeMap } from 'rxjs';
 
 @Component({
- selector: 'app-list-role',
-  standalone: true,
-  imports: [CommonModule],
- templateUrl: './list-role.component.html',
- styleUrl: './list-role.component.css'
+	selector: 'app-list-role',
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: './list-role.component.html',
+	styleUrl: './list-role.component.css'
 })
 export class ListRoleComponent extends BaseComponent implements OnInit {
-  rest_role: RestSimple<Role> = this.rest.initRestSimple('role', ['name', 'id', 'created', 'updated']);
-  search_role: SearchObject<Role> = this.rest_role.getEmptySearch();
-  role_list: Role[] = [];
+	rest_role: RestSimple<Role> = this.rest.initRestSimple('role', ['name', 'id', 'created', 'updated']);
+	search_role: SearchObject<Role> = this.rest_role.getEmptySearch();
+	role_list: Role[] = [];
 
-  ngOnInit() {
- this.sink = this.route.paramMap.pipe(
- mergeMap((paramMap) => {
- this.search_role = this.rest_role.getSearchObject(paramMap);
- this.search_role.limit = this.page_size;
- this.current_page = this.search_role.page;
- return this.rest_role.search(this.search_role);
- })
- ).subscribe((response) => {
- this.is_loading = false;
- this.role_list = response.data;
- this.setPages(this.current_page, response.total);
- });
-  }
+	ngOnInit()
+	{
+		this.sink = this.route.paramMap.pipe
+		(
+			mergeMap((paramMap) =>
+			{
+				this.search_role = this.rest_role.getSearchObject(paramMap);
+				this.search_role.limit = this.page_size;
+				this.current_page = this.search_role.page;
+				return this.rest_role.search(this.search_role);
+			})
+		).subscribe((response) => {
+				this.is_loading = false;
+				this.role_list = response.data;
+				this.setPages(this.current_page, response.total);
+			});
+	}
 }
