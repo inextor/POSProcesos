ab0c43ef8e9806627283c0a66f4e9eb572e5289e
nextor <nextor@gmail.com>
2025-07-28 13:49:58 -0700

    Removing spaces

--- src/app/modules/shared/RestModels.ts
+++ src/app/modules/shared/RestModels.ts
@@ -303,19 +303,19 @@ export interface Commanda_Type{
 	updated:Date;
 }
 export interface Consumption {
-  id: number;
-  item_id: number;
-  price: number;
-  qty: number;
-  production_area_id: number | null;
-  consumed_by_user_id: number | null;
-  store_id: number;
-  description: string | null;
-  status: 'ACTIVE' | 'DELETED';
-  created: Date;
-  created_by_user_id: number;
-  updated: Date;
-  updated_by_user_id: number;
+	id: number;
+	item_id: number;
+	price: number;
+	qty: number;
+	production_area_id: number | null;
+	consumed_by_user_id: number | null;
+	store_id: number;
+	description: string | null;
+	status: 'ACTIVE' | 'DELETED';
+	created: Date;
+	created_by_user_id: number;
+	updated: Date;
+	updated_by_user_id: number;
 }
 export interface Currency{
 	id:string;
