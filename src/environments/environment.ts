1dbae1639b1a228fdaea87bfa033609159f5b398
nextor <nextor@gmail.com>
2025-10-10 09:40:50 -0700

    environment update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
+    production: true,
     app_settings: {
 	//test_url: 'http://192.168.10.165',
 		socket_io_url: 'https://notifications.integranet.xyz:5000',
