b57d6d8b20333dfb3ac5315e940d61573d175103
Diego Armando Tapia Equihua <diego.tapia@uabc.edu.mx>
2025-12-08 17:04:25 -0800

    FUNCION PARA VACIAR AHORA CORRECTAMENTE LLAMANDO A UPDATES
    Se cambio para que ahora el componente en weird llame al endpoint que se encuentra dentro de la carpeta updates en el backend...

---
+++ src/app/components/clear-inventory/clear-inventory.component.ts
@@ -0,0 +1,98 @@
+
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { BaseComponent } from '../../modules/shared/base/base.component';
+import { Store } from '../../modules/shared/RestModels/Store';
+import { ModalComponent } from '../modal/modal.component';
+
+@Component({
+  selector: 'app-clear-inventory',
+  imports: [CommonModule, ModalComponent],
+  templateUrl: './clear-inventory.component.html',
+  styleUrl: './clear-inventory.component.css'
+})
+export class ClearInventoryComponent extends BaseComponent
+{
+  rest_store = this.rest.initRestSimple<Store>('store');
+  http!: HttpClient;
+  stores: Store[] = [];
+  selected_store_id: number | null = null;
+  show_confirmation_modal: boolean = false;
+
+  ngOnInit(): void
+  {
+    this.http = this.injector.get(HttpClient);
+    this.loadStores();
+  }
+
+  loadStores(): void
+  {
+    this.subs.sink = this.rest_store.search({limit: 99999, sort_order: ['name_ASC']}).subscribe({
+      next: (response) =>
+      {
+        this.stores = response.data;
+      },
+      error: (error) =>
+      {
+        this.showError(error);
+      }
+    });
+  }
+
+  onStoreChange(event: Event): void
+  {
+    let target = event.target as HTMLSelectElement;
+    this.selected_store_id = target.value ? parseInt(target.value) : null;
+  }
+
+  openConfirmationModal(): void
+  {
+    if (!this.selected_store_id)
+    {
+      this.showWarning('Por favor selecciona una sucursal');
+      return;
+    }
+
+    this.show_confirmation_modal = true;
+  }
+
+  clearInventory(): void
+  {
+    if (!this.selected_store_id)
+    {
+      this.showWarning('Por favor selecciona una sucursal');
+      return;
+    }
+
+    const data = { store_id: this.selected_store_id };
+
+    const url = `${this.rest.domain_configuration.domain}/${this.rest.url_base}/updates/clearStoreInventory.php`;
+
+    this.subs.sink = this.http.post<any>(url, data, {
+      headers: this.rest.getSessionHeaders(),
+      withCredentials: true
+    }).subscribe({
+      next: (response) =>
+      {
+        this.showSuccess('Inventario vaciado exitosamente');
+        this.show_confirmation_modal = false;
+        this.selected_store_id = null;
+      },
+      error: (error) =>
+      {
+        this.showError(error);
+        this.show_confirmation_modal = false;
+      }
+    });
+  }
+
+  getSelectedStoreName(): string
+  {
+    if (!this.selected_store_id)
+      return '';
+
+    let store = this.stores.find(s => s.id === this.selected_store_id);
+    return store ? store.name || '' : '';
+  }
+}
