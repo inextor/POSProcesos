91832d5f0a1d63a9c265b13239e95ac18d92b9eb
nextor <nextor@gmail.com>
2026-01-02 16:21:05 -0800

    Se corrigieron bugs y se probo

--- src/app/pages/item-movement-report/item-movement-report.component.ts
+++ src/app/pages/item-movement-report/item-movement-report.component.ts
@@ -37,6 +37,8 @@ export class ItemMovementReportComponent extends BaseComponent implements OnInit
 	rest_store: RestSimple<Store> = this.rest.initRestSimple('store', ['id', 'name', 'created', 'updated']);
 	stores: Store[] = [];
 	rest_category: RestSimple<Category> = this.rest.initRestSimple('category', ['id']);
+	sortColumn: string = '';
+	sortDirection: 'asc' | 'desc' = 'asc';
 
 	get totalReceived(): number
 	{
@@ -68,7 +70,7 @@ export class ItemMovementReportComponent extends BaseComponent implements OnInit
 				this.path = '/item-movement-report';
 				this.is_loading = true;
 
-				this.item_movement_search = this.getSearch(param_map, ['store_id','start','end']);
+				this.item_movement_search = this.getSearch(param_map, ['store_id','start_timestamp','end_timestamp']);
 
 				let start: Date = new Date();
 				let end: Date = new Date();
@@ -81,9 +83,9 @@ export class ItemMovementReportComponent extends BaseComponent implements OnInit
 				{
 					start = new Date();
 					start.setHours(0, 0, 0, 0);
+					this.item_movement_search.eq.start_timestamp = start;
 				}
 
-				this.item_movement_search.eq.start_timestamp = start;
 
 				if (this.item_movement_search.eq.end_timestamp )
 				{
@@ -108,8 +110,8 @@ export class ItemMovementReportComponent extends BaseComponent implements OnInit
 					stores: store_obs,
 					report: this.rest.getReportByPath('getItemMovement',
 					{
-						start_timestamp: this.start_date,
-						end_timestamp: this.end_date,
+						start_timestamp: start,
+						end_timestamp: end,
 						store_id: this.item_movement_search.eq['store_id'],
 					}) as Observable<RestResponse<ItemMovement>>
 				})
@@ -164,5 +166,66 @@ export class ItemMovementReportComponent extends BaseComponent implements OnInit
 	performSearch()
 	{
 		super.search(this.item_movement_search);
+	}
+
+	sortBy(column: string)
+	{
+		if (this.sortColumn === column)
+		{
+			this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+		}
+		else
+		{
+			this.sortColumn = column;
+			this.sortDirection = 'asc';
+		}
+
+		this.item_movement_list.sort((a, b) =>
+		{
+			let aValue: any;
+			let bValue: any;
+
+			switch (column)
+			{
+				case 'item_id':
+					aValue = a.item_id;
+					bValue = b.item_id;
+					break;
+				case 'item_name':
+					aValue = a.item_name?.toLowerCase() || '';
+					bValue = b.item_name?.toLowerCase() || '';
+					break;
+				case 'item_code':
+					aValue = a.item_code?.toLowerCase() || '';
+					bValue = b.item_code?.toLowerCase() || '';
+					break;
+				case 'category_name':
+					aValue = a.category?.name?.toLowerCase() || '';
+					bValue = b.category?.name?.toLowerCase() || '';
+					break;
+				case 'total_received':
+					aValue = a.total_received || 0;
+					bValue = b.total_received || 0;
+					break;
+				case 'total_requested':
+					aValue = a.total_requested || 0;
+					bValue = b.total_requested || 0;
+					break;
+				case 'total_merma':
+					aValue = a.total_merma || 0;
+					bValue = b.total_merma || 0;
+					break;
+				case 'total_sold':
+					aValue = a.total_sold || 0;
+					bValue = b.total_sold || 0;
+					break;
+				default:
+					return 0;
+			}
+
+			if (aValue < bValue) return this.sortDirection === 'asc' ? -1 : 1;
+			if (aValue > bValue) return this.sortDirection === 'asc' ? 1 : -1;
+			return 0;
+		});
 	}
 }
