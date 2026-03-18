9c0bfb7eb5f61eaeec1bce4f280bc1e758df30bb
nextor <nextor@gmail.com>
2026-03-18 10:12:34 -0700

    Se agrego la lista de inventario por lotes

--- src/app/pages/list-batch-record/list-batch-record.component.ts
+++ src/app/pages/list-batch-record/list-batch-record.component.ts
@@ -1,50 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { BaseComponent } from '../../modules/shared/base/base.component';
 import { Batch_Record, Store } from '../../modules/shared/RestModels';
-import { RestResponse, RestSimple, SearchObject } from '../../modules/shared/services/Rest';
-import { forkJoin, mergeMap } from 'rxjs';
-import { PaginationComponent } from "../../components/pagination/pagination.component";
+import { BatchRecordInfo } from '../../modules/shared/Models';
+import { Rest, RestSimple, SearchObject } from '../../modules/shared/services/Rest';
+import { forkJoin, switchMap } from 'rxjs';
+import { PaginationComponent } from '../../components/pagination/pagination.component';
+import { ShortDatePipe } from '../../modules/shared/pipes/short-date.pipe';
+import { LoadingComponent } from '../../components/loading/loading.component';
 
 @Component({
 	selector: 'app-list-batch-record',
 	standalone: true,
-	imports: [CommonModule, PaginationComponent],
+	imports: [CommonModule, FormsModule, RouterModule, PaginationComponent, ShortDatePipe, LoadingComponent],
 	templateUrl: './list-batch-record.component.html',
 	styleUrl: './list-batch-record.component.css'
 })
-export class ListBatchRecordComponent extends BaseComponent
+export class ListBatchRecordComponent extends BaseComponent implements OnInit
 {
+	rest_batch_record: Rest<Batch_Record, BatchRecordInfo> = this.rest.initRest<Batch_Record, BatchRecordInfo>(
+		'batch_record_info',
+		['id', 'batch', 'item_id', 'store_id', 'created', 'updated', 'movement_type', 'is_current']
+	);
+	rest_store: RestSimple<Store> = this.rest.initRestSimple<Store>('store', ['id', 'name']);
 
-	batch_record_list:Batch_Record[] = [];
-	rest_batch_record:RestSimple<Batch_Record> = this.rest.initRestSimple<Batch_Record>('batch_record',['id','batch','item_id','store_id','created','updated']);
-	rest_store:RestSimple<Store> = this.rest.initRestSimple<Store>('store',['id','name']);
-	batch_record_search:SearchObject<Batch_Record> = this.rest_batch_record.getEmptySearch();
+	batch_record_search: SearchObject<Batch_Record> = this.getEmptySearch();
+	batch_record_info_list: BatchRecordInfo[] = [];
 	store_list: Store[] = [];
-	fecha_inicial: string = '';
+
+	type_dic: Record<string, string> = {
+		'POSITIVE': 'Entrada',
+		'NEGATIVE': 'Salida',
+		'ADJUSTMENT': 'Ajuste'
+	};
 
 	ngOnInit()
 	{
-		this.sink = this.getQueryParamObservable()
-		.pipe
-		(
-			mergeMap(([_params, queries]) =>
-			{
-				this.batch_record_search = this.rest_batch_record.getSearchObject(queries);
-				return forkJoin
-				({
-					batch_records: this.rest_batch_record.searchAll(this.batch_record_search),
-					stores: this.rest_store.getAll()
-				});
+		this.path = '/list-batch-record';
+		this.is_loading = true;
+
+		this.sink = forkJoin({
+			stores: this.rest_store.getAll()
+		}).pipe(
+			switchMap((initial) => {
+				this.store_list = initial.stores.data;
+
+				return this.route.queryParamMap;
+			}),
+			switchMap((queryParamMap) => {
+				this.batch_record_search = this.getSearch<Batch_Record>(
+					queryParamMap,
+					['id', 'batch', 'item_id', 'store_id', 'created', 'updated', 'movement_type', 'is_current'],
+					['item_name', 'batch', 'is_current']
+				);
+
+				// Set default store_id if not set and user doesn't have global permission
+				if (!this.batch_record_search.eq.store_id || !this.rest.user_permission.global_add_stock) {
+					this.batch_record_search.eq.store_id = this.rest.user?.store_id??undefined;
+				}
+
+				this.batch_record_search.limit = this.page_size;
+				this.current_page = this.batch_record_search.page;
+
+				return this.rest_batch_record.search(this.batch_record_search);
 			})
-		)
-		.subscribe
-		({
-			error: (error) => this.rest.showError(error),
-			next: (response) =>
-			{
-				this.batch_record_list = response.batch_records.data;
-				this.store_list = response.stores.data;
+		).subscribe({
+			next: (response) => {
+				this.is_loading = false;
+				this.batch_record_info_list = response.data;
+				this.setPages(this.current_page, response.total);
+			},
+			error: (error) => {
+				this.is_loading = false;
+				this.rest.showError(error);
 			}
 		});
 	}
